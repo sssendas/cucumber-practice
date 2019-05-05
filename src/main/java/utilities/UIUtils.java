06261392b48b354ab57f3eb79c0bdacf56e08948
sssendas <sssendas@gmail.com.com>
2019-05-05 22:17:41 +0530

    Commit with chrome and firefox headless

--- src/main/java/utilities/UIUtils.java
+++ src/main/java/utilities/UIUtils.java
@@ -4,7 +4,9 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class UIUtils
@@ -19,7 +21,8 @@ public class UIUtils
 		String browser = prop.getProperty("browser");
 		System.out.println("Browser value provided as "+browser+" in property file");
 		
-		if(browser.equalsIgnoreCase("chrome")||browser.equalsIgnoreCase("firefox")||browser.equalsIgnoreCase("ie"))
+		if(browser.equalsIgnoreCase("chrome")||browser.equalsIgnoreCase("firefox")||browser.equalsIgnoreCase("ie")
+				||browser.equalsIgnoreCase("chromeheadless")||browser.equalsIgnoreCase("firefoxheadless"))
 		{
 			if(browser.equalsIgnoreCase("chrome"))
 			{
@@ -32,7 +35,7 @@ public class UIUtils
 			{
 				System.setProperty("webdriver.gecko.driver","src/main/resources/drivers/geckodriver.exe");
 				driver = new FirefoxDriver();
-				System.out.println("Initiated firefox-driver as driver");
+				System.out.println("Initiated Firefox-driver as driver");
 			}
 			
 			if(browser.equalsIgnoreCase("ie"))
@@ -40,6 +43,30 @@ public class UIUtils
 				System.setProperty("webdriver.ie.driver","src/main/resources/drivers/IEDriverServer.exe");
 				driver = new InternetExplorerDriver();
 				System.out.println("Initiated IE-driver as driver");
+			}
+			if(browser.equalsIgnoreCase("chromeheadless"))
+			{
+				System.setProperty("webdriver.chrome.driver","src/main/resources/drivers/chromedriver.exe");
+				ChromeOptions chops = new ChromeOptions();
+				chops.addArguments("--headless");
+				
+//				or
+//				chops.setHeadless(true);
+				
+				driver = new ChromeDriver(chops);
+				System.out.println("Initiated ChromeHedless-driver as driver");
+			}
+			if(browser.equalsIgnoreCase("firefoxheadless"))
+			{
+				System.setProperty("webdriver.gecko.driver","src/main/resources/drivers/geckodriver.exe");
+				FirefoxOptions ffops = new FirefoxOptions();
+				ffops.setHeadless(true);
+				
+//				or
+//				ffops.addArguments("--headless");
+				
+				driver = new FirefoxDriver(ffops);
+				System.out.println("Initiated FirefoxHeadless-driver as driver");
 			}
 		}
 		else
