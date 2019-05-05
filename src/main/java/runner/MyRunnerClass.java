25043f199c63d4b3a3e9c9e5cb2a84cc06818a75
sssendas <sssendas@gmail.com.com>
2019-05-05 22:53:35 +0530

    this with cucumber json reporting

--- src/main/java/runner/MyRunnerClass.java
+++ src/main/java/runner/MyRunnerClass.java
@@ -7,7 +7,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(features = {"src\\test\\java\\features\\loginTest.feature"},
 					glue = {"stepdefinitions"},
-					plugin = {"pretty","html:target/cucumber"})
+					plugin = {"pretty","html:target/cucumber","json:target/cucumber/Cucumber_json_report.json"})
 
 @Test
 public class MyRunnerClass extends AbstractTestNGCucumberTests
