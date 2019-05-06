b84fc830b9d2a66a6519911fdc8f74b6a5cdda9d
sssendas <sssendas@gmail.com.com>
2019-05-06 22:50:21 +0530

    Included some more cucumber options

--- src/main/java/runner/MyRunnerClass.java
+++ src/main/java/runner/MyRunnerClass.java
@@ -7,7 +7,9 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(features = {"src\\test\\java\\features\\loginTest.feature"},
 					glue = {"stepdefinitions"},
-					plugin = {"pretty","html:target/cucumber","json:target/cucumber/Cucumber_json_report.json"})
+					plugin = {"pretty","html:target/cucumber","json:target/cucumber/Cucumber_json_report.json"},
+					dryRun = false,
+					monochrome = true)
 
 @Test
 public class MyRunnerClass extends AbstractTestNGCucumberTests
